223fe248066f27200032ace7081abd97db02dc92
Steve Souza <steve@stevesouza.com>
2018-06-12 14:10:37 -0400

    Added root element that allows jaxb to return as xml
    this hasn't worked yet. note annotation is not supposed to be needed if you use jackson xml dataformat, but didn't get that to work either.

--- src/main/java/com/stevesouza/HelloController.java
+++ src/main/java/com/stevesouza/HelloController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class HelloController {
 		@Autowired
 		private AppProps props;
 		
-	    @RequestMapping()
+	    @GetMapping()
 	    public List<Person> index() {
 	    	List<Person> people=new ArrayList<Person>();
 	    	people.add(new Person("Joe", "Souza"));
@@ -28,7 +29,7 @@ public class HelloController {
 	        return people;
 	    }
 	    
-	    @RequestMapping("/person/{personId}")
+	    @GetMapping("/person/{personId}")
 	   // @RequestMapping(value = "/person", method = RequestMethod.GET, produces = {"application/json"})
 	    public Person person(@PathVariable("personId") String personId) {
 	        return new Person(personId, props.getName());
