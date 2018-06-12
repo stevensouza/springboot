47e6b6b4fc719fc73a972f0f1d08efb3c3b36bf9
Steve Souza <steve@stevesouza.com>
2018-06-12 13:04:32 -0400

    Added rest method that returns a collection

--- src/main/java/com/stevesouza/HelloController.java
+++ src/main/java/com/stevesouza/HelloController.java
@@ -1,8 +1,13 @@
 package com.stevesouza;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/restapi")
@@ -12,14 +17,22 @@ public class HelloController {
 		private AppProps props;
 		
 	    @RequestMapping()
-	    public String index() {
-	        return "Greetings from Spring Boot and Steve!";
+	    public List<Person> index() {
+	    	List<Person> people=new ArrayList<Person>();
+	    	people.add(new Person("Joe", "Souza"));
+	    	people.add(new Person("Winnie", "Souza"));
+	    	people.add(new Person("Sandra", "Souza"));
+	    	people.add(new Person("Jean", "Souza"));
+	    	people.add(new Person("Joel", "Souza"));
+	    	people.add(new Person("Steven", "Souza"));
+	        return people;
 	    }
 	    
-	    @RequestMapping("/person")
+	    @RequestMapping("/person/{personId}")
 	   // @RequestMapping(value = "/person", method = RequestMethod.GET, produces = {"application/json"})
-	    public Person person() {
-	        return new Person("Steve", props.getName());
+	    public Person person(@PathVariable("personId") String personId) {
+	        return new Person(personId, props.getName());
 	    }
+	    
 
 }
